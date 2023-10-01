eebd62c0ea6e9ae7063044c85f8daee3dbefb92b
LeiWithP <pedro.barrita1029@gmail.com>
2023-10-01 13:57:23 -0600

    Option Title editable

--- material-example/src/components/TabNavigator.jsx
+++ material-example/src/components/TabNavigator.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Screen1 from "../screens/Screen1";
 import Screen2 from "../screens/Screen2";
@@ -7,11 +8,24 @@ import Screen3 from "../screens/Screen3";
 const Tab = createMaterialTopTabNavigator();
 
 const TabNavigator = () => {
+  const [isTitleVisible, setIsTitleVisible] = useState(false);
+
+  const toggleTitleVisibility = () => {
+    setIsTitleVisible(!isTitleVisible);
+  };
+
   return (
-    <Tab.Navigator style={{ marginTop: 30 }}>
-      <Tab.Screen name="Home" component={Screen1} />
-      <Tab.Screen name="Second" component={Screen2} />
-      <Tab.Screen name="Third" component={Screen3} />
+    <Tab.Navigator
+      style={{ marginTop: 30 }}
+      screenOptions={({ route }) => ({
+        title: isTitleVisible ? "Hello" : route.name,
+      })}
+    >
+      <Tab.Screen name="Props" component={Screen1} />
+      <Tab.Screen name="Options">
+        {() => <Screen2 toggleTitle={toggleTitleVisibility} />}
+      </Tab.Screen>
+      <Tab.Screen name="Events & Helpers" component={Screen3} />
     </Tab.Navigator>
   );
 };
